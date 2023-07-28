2ebe9a0f1e02fbf603bbacafc5c3aadb9f66e709
roddg <roddg@mail.ru>
2023-07-28 12:24:33 +0900

    task 4 edit

--- lesson-3/Java/task4/Main.java
+++ lesson-3/Java/task4/Main.java
@@ -2,15 +2,15 @@ package task4;
 
 public class Main {
     public static void main(String[] args) {
-        //Workable worker = new RobotWorker();
+        Workable worker = new RobotWorker();
 
-        HumanWorker worker = new HumanWorker();
+        //HumanWorker worker = new HumanWorker();
         worker.work();
 
         // Проверка типа объекта перед вызовом метода eat()
         if (worker instanceof Eatable) {
             Eatable eatableWorker = (Eatable) worker;
-            worker.eat();
+            eatableWorker.eat();
         } else {
             throw new UnsupportedOperationException("Роботы не едят!");
         }
